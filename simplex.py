0773b2bf5a310da25dda62e5f6b8abed4dfb7e83
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2022-11-13 20:20:27 +0000

    fixed floating point error - introduced err

--- simplex.py
+++ simplex.py
@@ -16,7 +16,7 @@ def swapIn(tableu: np.ndarray, indIn: int ,indOut: int):
             extMatrix[i] -= extMatrix[indOut + 1] * extMatrix[i, indIn]
                 
 
-def simplex(tableu: np.ndarray, zeroIndexed=True, printIntermediateTableu=False, blandsRule=False) -> np.ndarray:
+def simplex(tableu: np.ndarray, zeroIndexed=True, printIntermediateTableu=False, blandsRule=False, err: float = 1e-9) -> np.ndarray:
 
     z = tableu[0, 1:-1]
     basic = tableu[1:, 0]
@@ -35,13 +35,16 @@ def simplex(tableu: np.ndarray, zeroIndexed=True, printIntermediateTableu=False,
             indIn = np.argsort(z[:-1])[::-1]
             for ind in indIn: 
                 arr = [(cost[k+1] / i if i > 0 else np.inf) for k, i in enumerate(matrix[:, ind])]
-                if z[:-1][ind] <= 0:
+                if z[:-1][ind] <= err:
                     return basic + int(zeroIndexed), cost[1:] , cost[0]
                 if np.all(np.array(arr) == np.inf):
                     continue
                     # return basic + int(zeroIndexed), cost[1:] , cost[0]
                 else:
                     break
+            if z[:-1][ind] <= err:
+                return basic + int(zeroIndexed), cost[1:] , cost[0]
+                
             indOut = np.argmin(arr)
             indIn = ind
             
@@ -53,15 +56,15 @@ def simplex(tableu: np.ndarray, zeroIndexed=True, printIntermediateTableu=False,
             indIn = range(len(z[:-1]))
             for ind in indIn: 
                 arr = [(cost[k+1] / i if i > 0 else np.inf) for k, i in enumerate(matrix[:, ind])]
-                if z[:-1][ind] <= 0:
+                if z[:-1][ind] <= err or ind in tableu[1:,0]:
                     continue
                 if np.all(np.array(arr) == np.inf):
                     continue
                     # return basic + int(zeroIndexed), cost[1:] , cost[0]
                 else:
                     break
-
-            if z[:-1][ind] <= 0:
+                
+            if z[:-1][ind] <= err:
                 return basic + int(zeroIndexed), cost[1:] , cost[0]
                 
             indOut = np.argmin(arr)
@@ -71,10 +74,8 @@ def simplex(tableu: np.ndarray, zeroIndexed=True, printIntermediateTableu=False,
             return simplex(tableu, zeroIndexed, printIntermediateTableu, blandsRule)
         
 
-# np.set_printoptions(precision=4, suppress=True, linewidth=10000)
+np.set_printoptions(precision=4, suppress=True, linewidth=10000)
 
 if __name__ == "__main__":
     pass
-
-
-    
+   
